b14bd330841b82ad4370019709a7c4717ff1a7fc
Itamarshahar <78688846+Itamarshahar@users.noreply.github.com>
2023-09-05 11:23:38 +0300

    Update to README

--- run_visualization_topic_model.R
+++ run_visualization_topic_model.R
@@ -161,27 +161,7 @@ ggsave("DotPlot_topics_vs_celltype.pdf",
        path=path_to_plots,
 )
 
-################################################################################
-## run DE
-################################################################################
 
-
-set.seed(1)
-counts <- obj@assays$RNA@counts
-row_sum <- rowSums(counts)
-nonzero_rows <- row_sum != 0
-counts <- counts[nonzero_rows,]
-counts <- t(counts)
-
-de_psd <- de_analysis(fit = fit,
-                  X = counts,
-                  pseudocount = 0.1,
-                  lfc.stat  = "le",
-                  control = list(ns = 1e4,nc = 6),
-                  verbose = TRUE,
-                  )
-setwd("/Users/itamar_shahar/Library/CloudStorage/GoogleDrive-itamar.shahar2@mail.huji.ac.il/My Drive/University/General/3rd_year_project/databases/")
-saveRDS(de_psd, "de_night")
 
 ################################################################################
 ## visualize DE
