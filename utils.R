131b01a6e4d539be0f9957d9933973612b51603f
shmuel-choen <shmuelkohen1@gmail.com>
2024-02-21 10:13:56 +0200

    util

--- utils.R
+++ utils.R
@@ -209,3 +209,19 @@ generate_ScatterPlot_DE <- function(de_enriched,
            width = 10,
            plot = ., limitsize = FALSE)
 }
+
+######## set sample and diagnosis in order #######  "AD_7264-2"
+set_sample_and_diagnosis_order <- function(obj,
+                                           sample_order = c("AD_6998", "AD_7258","AD_7264-1", "AD_7264-2", "AD_7436",
+                                                            "MCI_6962", "MCI_6991", "MCI_6992", "MCI_7162",
+                                                            "HA_7182","HA_7426", "HA_8126","HA_UWA-747",
+                                                            "SuperAgers_A-19-08", "SuperAgers_A-19-79", "SuperAgers_A20-22", "SuperAgers_A21-135",
+                                                            "Young CTRL_7001","Young CTRL_7320"),
+                                           diagnosis_order = c("AD", "MCI", "HA", "SuperAgers", "Young CTRL")) {
+  
+  obj@meta.data$Diagnosis_SampleID <- factor(x = obj@meta.data$Diagnosis_SampleID, levels = sample_order)
+  obj@meta.data$Diagnosis <- factor(x = obj@meta.data$Diagnosis, levels = diagnosis_order)
+  
+  obj
+}
+
